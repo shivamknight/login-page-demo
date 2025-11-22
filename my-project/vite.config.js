981b511bea289ae18d222645de8f7ae4cde57335
shivam kumar <knightshivam39@gmail.com>
2025-11-22 14:51:00 +0530

    Update vite.config.js

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-})
-export default {
   base: "/login-page-demo/",
-}
+})
